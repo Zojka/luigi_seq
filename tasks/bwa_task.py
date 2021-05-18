b07bc9112d9778cbf9c662e8b7a39ba95eedc6d8
Zparteka <z.parteka@cent.uw.edu.pl>
2021-05-18 16:47:10 +0200

    task: mapping quality

--- tasks/bwa_task.py
+++ tasks/bwa_task.py
@@ -32,7 +32,7 @@ class RemoveNotAlignedReads(luigi.Task):
     reference = luigi.Parameter()
     outname = luigi.Parameter(default="output.bam")
 
-    outname_mapped = "output_mapped.bam"
+    outname_mapped = luigi.Parameter(default="output_mapped.bam")
 
     def requires(self):
         return Mapping(r1=self.r1, r2=self.r2, threads=self.threads, reference=self.reference, outname=self.outname)
@@ -44,6 +44,23 @@ class RemoveNotAlignedReads(luigi.Task):
         samtools = local["samtools"]
         (samtools["view", "-F", "0x04", "-b", self.outname] > self.outname_mapped)()
 
+class MappingQualityFilter(luigi.Task):
+    r1 = luigi.Parameter()
+    r2 = luigi.Parameter()
+    threads = luigi.Parameter()
+    reference = luigi.Parameter()
+    outname = luigi.Parameter(default="output.bam")
+
+    outname_mapped = luigi.Parameter(default="output_mapped.bam")
+    outname_filtered = "output_mapped_filtered.bam"
+    quality = luigi.Parameter(default=30)
+
+    def requires(self):
+        return RemoveNotAlignedReads(r1=self.r1, r2=self.r2, threads=self.threads, reference=self.reference, outname=self.outname)
+
+    def run(self):
+        samtools = local["samtools"]
+        (samtools["view", "-q", self.quality, "-t", self.threads, "-b", self.outname_mapped] > self.outname_filtered)()
 
 if __name__ == '__main__':
     luigi.build()
