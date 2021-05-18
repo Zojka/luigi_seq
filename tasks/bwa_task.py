0596e5cfc1abe6fb3ddf6965a60f072366fddda2
Zparteka <z.parteka@cent.uw.edu.pl>
2021-05-18 13:47:15 +0200

    testing subprocess

--- tasks/bwa_task.py
+++ tasks/bwa_task.py
@@ -5,22 +5,23 @@
 """
 
 import luigi
-from os import system
+import subprocess
 from luigi.contrib.external_program import ExternalProgramTask, ExternalPythonProgramTask
 from luigi.contrib.external_program import ExternalProgramRunError
 
 
-class Mapping(ExternalProgramTask):
+class Mapping(luigi.Task):
     r1 = luigi.Parameter()
     r2 = luigi.Parameter()
     threads = luigi.Parameter()
     reference = luigi.Parameter()
 
-    def program_args(self):
-        return ["bwa", "mem", "-SP5M", f"-t{self.threads}", self.reference, self.r1, self.r2]
-
     def output(self):
         return luigi.LocalTarget("output.bam")
+
+    def run(self):
+        subprocess.run(
+            ["bwa", "mem", "-SP5M", f"-t{self.threads}", self.reference, self.r1, self.r2, ">", self.output()])
 
 
 if __name__ == '__main__':
