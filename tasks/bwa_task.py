dc523d83389cc33fea9000775fe38829d2843175
Zparteka <z.parteka@cent.uw.edu.pl>
2021-05-18 14:43:42 +0200

    testing plumbum

--- tasks/bwa_task.py
+++ tasks/bwa_task.py
@@ -6,8 +6,6 @@
 
 import luigi
 from plumbum import local
-from luigi.contrib.external_program import ExternalProgramTask, ExternalPythonProgramTask
-from luigi.contrib.external_program import ExternalProgramRunError
 
 
 class Mapping(luigi.Task):
@@ -15,14 +13,28 @@ class Mapping(luigi.Task):
     r2 = luigi.Parameter()
     threads = luigi.Parameter()
     reference = luigi.Parameter()
+    outname = luigi.Parameter(default="output.bam")
 
     def output(self):
-        return luigi.LocalTarget("output.bam")
+        return luigi.LocalTarget(self.outname)
 
     def run(self):
         bwa = local["bwa"]
-        (bwa["mem", "-SP5M", f"-t{self.threads}", self.reference, self.r1, self.r2] > "output.bam")()
+        (bwa["mem", "-SP5M", f"-t{self.threads}", self.reference, self.r1, self.r2] > self.outname)()
+"""
 
+class SamtoolsView(luigi.Task):
 
+    def requires(self):
+        return Mapping()
+
+    def output(self):
+        return luigi.LocalTarget(input())
+
+    def run(self):
+        samtools = local["samtools"]
+        (samtools["view", "--bhS", self.input()] > "output.bam")()
+
+"""
 if __name__ == '__main__':
     luigi.run()
