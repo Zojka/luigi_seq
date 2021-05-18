2f2118da47e005d83a9e09f7b4f8273a190a63fb
Zparteka <z.parteka@cent.uw.edu.pl>
2021-05-18 13:56:41 +0200

    testing plumbum

--- tasks/bwa_task.py
+++ tasks/bwa_task.py
@@ -21,7 +21,7 @@ class Mapping(luigi.Task):
 
     def run(self):
         bwa = local["bwa"]
-        bwa["mem", "-SP5M", f"-t{self.threads}", self.reference, self.r1, self.r2] > self.output()
+        bwa["mem", "-SP5M", f"-t{self.threads}", self.reference, self.r1, self.r2] > "output.bam"
 
 
 if __name__ == '__main__':
