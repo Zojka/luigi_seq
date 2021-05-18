ff76a9f44a0213c3a0d706a595703ac129e285a2
Zparteka <z.parteka@cent.uw.edu.pl>
2021-05-18 15:32:10 +0200

    testing plumbum

--- tasks/bwa_task.py
+++ tasks/bwa_task.py
@@ -21,8 +21,24 @@ class Mapping(luigi.Task):
     def run(self):
         bwa = local["bwa"]
         samtools = local["samtools"]
-        (bwa["mem", "-SP5M", f"-t{self.threads}", self.reference, self.r1, self.r2] | samtools["view", "-bhS"] > self.outname)()
+        (bwa["mem", "-SP5M", f"-t{self.threads}", self.reference, self.r1, self.r2] | samtools[
+            "view", "-bhS"] > self.outname)()
 
-#class RemoveNotAlignedReads(luigi.Task):
+
+class RemoveNotAlignedReads(luigi.Task):
+    inp = Mapping.outname
+    outname = Mapping.outname[:-4] + "_mapped.bam"
+
+    def requires(self):
+        return Mapping()
+
+    def output(self):
+        return luigi.LocalTarget(self.outname)
+
+    def run(self):
+        samtools = local["samtools"]
+        (samtools["view", "-F", "0x04", "-b", self.input()] > self.outname)()
+
+
 if __name__ == '__main__':
     luigi.run()
