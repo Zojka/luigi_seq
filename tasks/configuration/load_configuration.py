aab9c80cf2a0d79184cdbef901d319129fcdea8c
Zparteka <z.parteka@cent.uw.edu.pl>
2021-08-02 12:21:34 +0200

    debug

--- tasks/configuration/load_configuration.py
+++ tasks/configuration/load_configuration.py
@@ -40,7 +40,7 @@ class Configuration:
         self.outnames["index"] = join(self.outdir, f"{base}_indexed.bam")
         self.outnames["sorted"] = join(self.outdir, f"{base}_sorted.bam")
         self.outnames["peaks"] = join(self.outdir, f"{base}_macs3")
-        self.outnames["maps"] = join(self.outdir, f"MAPS_output/{self.maps_dataset}_current/{self.maps_dataset}.5k.2.sig3Dinteractions.bedpe ")
+        self.outnames["maps"] = join(self.outdir, f"MAPS_output/{self.maps_dataset}_current/{self.maps_dataset}.5k.2.sig3Dinteractions.bedpe")
         print(self.outnames["maps"])
 
     def dumps(self):
