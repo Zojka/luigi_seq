a096ab48804f8b0aa4eac370d25195d3fd3599f2
Zparteka <z.parteka@cent.uw.edu.pl>
2022-05-12 13:03:16 +0200

    shallow sample

--- tasks/configuration/chipseq_configuration.py
+++ tasks/configuration/chipseq_configuration.py
@@ -175,8 +175,8 @@ input_gm12878 = {"gm12878_sa1": [("/mnt/raid/zparteka/cohesin_hichip_project/gm1
 chips = chips_gm_pooled
 input = input_gm_pooled
 samples = samples_mumbach
-sample_arima_hichip = {"sample": [("/mnt/raid/zparteka/hichip/swift/raw_data/fastq/KJ_Swift_1_R1.fastq.gz",
-                                   "/mnt/raid/zparteka/hichip/swift/raw_data/fastq/KJ_Swift_1_R2.fastq.gz")]}
+sample_arima_hichip = {"sample": [("/mnt/raid/zparteka/cohesin_hichip_project/gm12878_shallow/fastq/KJ_Swift_1_R1.fastq.gz",
+                                   "/mnt/raid/zparteka/cohesin_hichip_project/gm12878_shallow/fastq/KJ_Swift_1_R2.fastq.gz")]}
 samples = sample_arima_hichip
 
 class Configuration:
