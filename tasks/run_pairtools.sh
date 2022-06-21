cd10d5b8084d5077132328591e1d744d998bd4bc
Zparteka <z.parteka@cent.uw.edu.pl>
2022-06-21 08:21:40 +0200

    hichip mapping - debug

--- tasks/run_pairtools.sh
+++ tasks/run_pairtools.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 COMPRESS_PROGRAM=gzip
 
-pairtools parse $MAPPED -c $CHROMOSOMES --add-columns mapq | pairtools sort --nproc ${THREADS} --memory ${MEM} --compress-program ${COMPRESS_PROGRAM}
-                                                         --tmpdir ${TMPDIR} --output ${SORTED_PAIRS_PATH}
+pairtools parse $MAPPED -c $CHROMOSOMES --add-columns mapq | pairtools sort --nproc ${THREADS} --tmpdir ${TMPDIR} --output ${SORTED_PAIRS_PATH}
