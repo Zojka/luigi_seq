2de5bae7980388c562fd42dfc52588c533176eb8
Zparteka <z.parteka@cent.uw.edu.pl>
2022-06-21 08:23:07 +0200

    hichip mapping - debug

--- tasks/run_pairtools.sh
+++ tasks/run_pairtools.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 COMPRESS_PROGRAM=gzip
 
-pairtools parse $MAPPED -c $CHROMOSOMES --add-columns mapq | pairtools sort --nproc ${THREADS} --tmpdir ${TMPDIR} --output ${SORTED_PAIRS_PATH}
+pairtools parse $MAPPED -c $CHROMOSOMES --add-columns mapq | pairtools sort --nproc 5 --tmpdir ${TMPDIR} --output ${SORTED_PAIRS_PATH}
