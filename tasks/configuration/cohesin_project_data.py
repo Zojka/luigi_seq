0543eb1a5eecc148c866abe42ac903ef34b62ed1
Zparteka <z.parteka@cent.uw.edu.pl>
2022-06-24 12:17:15 +0200

    added cohesin project samples

--- tasks/configuration/cohesin_project_data.py
+++ tasks/configuration/cohesin_project_data.py
@@ -6,14 +6,14 @@
 
 """data for cohesin project calculations. All files are located on team-bilbo worskation on MiNI WUT"""
 
-cohesin_project = {"gm12878_ctcf_hichip_mumbach": [("/mnt/raid/zparteka/cohesin_hichip_project/gm12878_ctcf_hichip_mumbach/rep1/SRR7990656_R1.fastq.gz",
-                                                    "/mnt/raid/zparteka/cohesin_hichip_project/gm12878_ctcf_hichip_mumbach/rep1/SRR7990656_R2.fastq.gz"),
-                                                   "/mnt/raid/zparteka/cohesin_hichip_project/gm12878_ctcf_hichip_mumbach/rep2/SRR7990657_R1.fastq.gz",
-                                                   "/mnt/raid/zparteka/cohesin_hichip_project/gm12878_ctcf_hichip_mumbach/rep2/SRR7990657_R2.fastq.gz"],
-                   "HCC1599_untreated_SMC1a_HiChIP": [("/mnt/raid/zparteka/cohesin_hichip_project/HCC1599_untreated_SMC1a_HiChIP/SRR7505611_R1.fastq.gz",
-                                                    "/mnt/raid/zparteka/cohesin_hichip_project/HCC1599_untreated_SMC1a_HiChIP/SRR7505611_R2.fastq.gz")],
-                   "Rec1_untreated_SMC1a_HiChIP": [("/mnt/raid/zparteka/cohesin_hichip_project/Rec1_untreated_SMC1a_HiChIP/SRR7505646_R1.fastq.gz",
-                                                    "/mnt/raid/zparteka/cohesin_hichip_project/Rec1_untreated_SMC1a_HiChIP/SRR7505646_R2.fastq.gz")]
+cohesin_project = {"gm12878_ctcf_hichip_mumbach": [("/mnt/raid/zparteka/cohesin_hichip_project/gm12878_ctcf_hichip_mumbach/rep1/fastq/SRR7990656_R1.fastq.gz",
+                                                    "/mnt/raid/zparteka/cohesin_hichip_project/gm12878_ctcf_hichip_mumbach/rep1/fastq/SRR7990656_R2.fastq.gz"),
+                                                   "/mnt/raid/zparteka/cohesin_hichip_project/gm12878_ctcf_hichip_mumbach/rep2/fastq/SRR7990657_R1.fastq.gz",
+                                                   "/mnt/raid/zparteka/cohesin_hichip_project/gm12878_ctcf_hichip_mumbach/rep2/fastq/SRR7990657_R2.fastq.gz"],
+                   "HCC1599_untreated_SMC1a_HiChIP": [("/mnt/raid/zparteka/cohesin_hichip_project/HCC1599_untreated_SMC1a_HiChIP/fastq/SRR7505611_R1.fastq.gz",
+                                                    "/mnt/raid/zparteka/cohesin_hichip_project/HCC1599_untreated_SMC1a_HiChIP/fastq/SRR7505611_R2.fastq.gz")],
+                   "Rec1_untreated_SMC1a_HiChIP": [("/mnt/raid/zparteka/cohesin_hichip_project/Rec1_untreated_SMC1a_HiChIP/fastq/SRR7505646_R1.fastq.gz",
+                                                    "/mnt/raid/zparteka/cohesin_hichip_project/Rec1_untreated_SMC1a_HiChIP/fastq/SRR7505646_R2.fastq.gz")]
                    }
 
 sample_arima_hichip_shallow = {"sample": [("/mnt/raid/zparteka/cohesin_hichip_project/gm12878_shallow/fastq/KJ_Swift_1_R1.fastq.gz",
