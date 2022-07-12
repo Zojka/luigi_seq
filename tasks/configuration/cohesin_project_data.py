06f896085a98165e463f73a46f38e0efffae406d
Zparteka <z.parteka@cent.uw.edu.pl>
2022-07-12 10:39:52 +0200

    added mumbach arwena

--- tasks/configuration/cohesin_project_data.py
+++ tasks/configuration/cohesin_project_data.py
@@ -15,10 +15,14 @@ cohesin_project = {"gm12878_ctcf_hichip_mumbach": [("/mnt/raid/zparteka/cohesin_
                    "Rec1_untreated_SMC1a_HiChIP": [("/mnt/raid/zparteka/cohesin_hichip_project/Rec1_untreated_SMC1a_HiChIP/fastq/SRR7505646_R1.fastq.gz",
                                                     "/mnt/raid/zparteka/cohesin_hichip_project/Rec1_untreated_SMC1a_HiChIP/fastq/SRR7505646_R2.fastq.gz")]
                    }
+cohesin_mumbach_arwena = {"gm12878_cohesin_hichip_mumbach":[("/mnt/raid/zparteka/hichip/mumbach/rep1/fastq/mumbach_rep1_pulled_R1.fastq",
+                                                             "/mnt/raid/zparteka/hichip/mumbach/rep1/fastq/mumbach_rep1_pulled_R2.fastq"),
+                                                            ("/mnt/raid/zparteka/hichip/mumbach/rep2/fastq/mumbach_rep2_pulled_R1.fastq",
+                                                             "/mnt/raid/zparteka/hichip/mumbach/rep2/fastq/mumbach_rep2_pulled_R2.fastq")]}
 
 sample_arima_hichip_shallow = {"sample": [("/mnt/raid/zparteka/cohesin_hichip_project/gm12878_shallow/fastq/KJ_Swift_1_R1.fastq.gz",
                                            "/mnt/raid/zparteka/cohesin_hichip_project/gm12878_shallow/fastq/KJ_Swift_1_R2.fastq.gz")]}
 sample_arima_hichip_deep = {"sample": [("/mnt/raid/zparteka/cohesin_hichip_project/karolina_arima/raw_data/fastq/KJ_Swift_1_S12_R1.fastq.gz",
                                         "/mnt/raid/zparteka/cohesin_hichip_project/karolina_arima/raw_data/fastq/KJ_Swift_1_S12_R2.fastq.gz")]}
 
-samples = cohesin_project
+samples = cohesin_mumbach_arwena
