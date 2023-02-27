ffe173b15f29f7fdc08f7936d4ef96c9692040d5
zojka <zofia.tojek@gmail.com>
2023-02-27 11:38:45 +0100

    fixing load_configuration

--- tasks/configuration/load_configuration.py
+++ tasks/configuration/load_configuration.py
@@ -82,9 +82,8 @@ class Configuration:
     threads = 33
     reference = "/mnt/raid/zparteka/hg38_reference/Homo_sapiens_assembly38.fasta"
     mapq = 30
-    peak_quality = 0.01
-    outnames = {"mapped": None, "mapped_only": None, "filtered": None, "nodup": None, "bigwig": None, "index:": None,
-                "sorted": None, "peaks": None}
+    peak_quality = 0.05
+    genome_size = "hs"
 
     def __init__(self, r1, r2):
         self.r1 = r1
@@ -95,11 +94,15 @@ class Configuration:
             makedirs(self.outdir)
         self.maps_dataset = basename(r1).split("_R")[0]
         self.fastq_dir = dirname(r1)
+        # self.bwa_index = "/mnt/raid/zparteka/mm10_genome/bwa/mm10.fa"
         self.bwa_index = "/mnt/raid/zparteka/hg38_reference/Homo_sapiens_assembly38.fasta"
+        self.outnames = {}
         self.create_outnames()
         self.narrow_peak = self.outnames["peaks"] + "_peaks.narrowPeak"
+        self.chromosomes = "/mnt/raid/zparteka/hg38_reference/hg38_chrom.sizes"
+        self.memory = "48G"
 
-    # todo add checking if value is None
+
     def create_outnames(self):
         base = basename(self.r1.split("_R1")[0])
         self.outnames["mapped"] = join(self.outdir, f"{base}.bam")
